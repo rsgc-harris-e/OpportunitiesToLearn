677a14a6c5053fa842a0327efe9ddb53b5dd7172
Student <student@macbook-pro-121-harris.local>
2016-12-07 14:05:13 -0500

    mario code finished and also instances playground was inputted in. Still need to do boogie bot.

--- 7-13-Mario/Mario.playground/Contents.swift
+++ 7-13-Mario/Mario.playground/Contents.swift
@@ -11,4 +11,27 @@
  */
 
 // Add your code below
-print("Hello world")
+func levelCost(heights: [Int], maxJump: Int)->Int{
+    var energy : Int = 0
+
+    
+    for i in 0...heights.count - 2{
+      let currentPlatformheight = heights[i]
+           let nextPlatformheight = heights[i + 1]
+        let heightdifference = abs(currentPlatformheight - nextPlatformheight)
+        if heightdifference == 0{
+            energy += 1
+            }else {
+            if heightdifference > maxJump {
+                return -1
+        }else {
+                energy += heightdifference * 2
+        }
+    }
+}
+    return energy
+}
+levelCost(heights: [1,2,2], maxJump: 5)
+
+
+
